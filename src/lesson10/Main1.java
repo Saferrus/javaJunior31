40f7ca4710349daaf4c70fb9e5980cb57985b409
nikas <nikas1994@inbox.ru>
2022-06-07 15:37:39 +0300

    Исправлен баг в lesson 10

--- src/lesson10/Main1.java
+++ src/lesson10/Main1.java
@@ -5,15 +5,15 @@ public class Main1 {
 
         Matrix test = new Matrix(3, 3);
 
-        double[][] num1 = {{2, 5, 6, 7},
-                {4, 6, 8, 9},
-                {7, 1, 3, 7}};
+        double[][] num1 = {  {2, 5, 6},
+                             {4, 6, 8},
+                             {7, 1, 3}};
 
         Matrix matrix1 = new Matrix(num1);
 
-        double[][] num2 = { {1, 3, 4, 9},
-                            {5, 4, 1, 3},
-                            {2, 4, 6, 5}};
+        double[][] num2 = { {1, 3, 4 },
+                            {5, 4, 1},
+                            {2, 4, 6}};
 
         double[][] num3 = {  {1, 0, 0},
                              {0,1,0},
@@ -21,9 +21,7 @@ public class Main1 {
 
         Matrix matrix2 = new Matrix(num2);
 
-        Matrix matrix3 = new Matrix(num3);
-        boolean a = matrix3.isIdentityMatrix();
-        System.out.println(a);
-
+       Matrix result = matrix1.mul(matrix2);
+       result.printToConsole();
     }
 }
--- src/lesson10/Matrix.java
+++ src/lesson10/Matrix.java
@@ -1,7 +1,5 @@
 package lesson10;
 
-import java.util.Scanner;
-
 public class Matrix implements IMatrix {
     private double[][] numbers;
 
@@ -101,11 +99,14 @@ public class Matrix implements IMatrix {
             return null;
         }
         Matrix result = new Matrix(this.getRows(), this.getColumns());
-        for (int i = 0; i < result.getRows(); i++) {
-            for (int j = 0; j < result.getColumns(); j++) {
-                result.setValueAt(i, j, this.getValueAt(i, j) * otherMatrix.getValueAt(i, j));
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < otherMatrix.getColumns(); j++) {
+                for (int k = 0; k < this.getColumns(); k++) { // aColumn
+                    result.setValueAt(i,j,(result.getValueAt(i,j) + this.getValueAt(i,k)* otherMatrix.getValueAt(k,j)));
+                }
             }
         }
+
         return result;
     }
 
@@ -142,11 +143,61 @@ public class Matrix implements IMatrix {
 
 
     }
+    //Метод для вычисления детерминанта
+    private Matrix arrayOfRange(int n, int m) {
+
+        Matrix matrix = new Matrix(this.getRows() - 1, this.getColumns() - 1);
+        int row_sub = 0;
+
+        for (int row = 0; row < this.getRows(); row++) {
+
+            int col_sub = 0;
+            if (row != n) {
+
+                for (int col = 0; col < numbers[row].length; col++) {
+
+                    if (col != m) {
+                        matrix.setValueAt(row - row_sub, col - col_sub, this.getValueAt(row, col));
+                    } else {
+                        col_sub = 1;
+                    }
+
+                }
+
+            } else {
+                row_sub = 1;
+            }
+
+        }
+
+        return matrix;
+    }
 
     @Override
     public double determinant() {
+        if (this.getRows() != this.getColumns()) {
+            System.out.println("Матрица не квадратная");
+            return 0;
+        }
+        if (this.getRows() == 2) {
+            return (getValueAt(0, 0) * getValueAt(1, 1)) - (getValueAt(0, 1) * getValueAt(1, 0));
+        } else {
+            int total = 0;
+            for (int n = 0; n < this.getRows(); n++) {
 
-        return 0;
+                // Get the array excluding the first row and current column (n)
+                Matrix matrix = this.arrayOfRange(0, n);
+
+                if (n % 2 == 0) {
+                    total += this.getValueAt(0, n) * matrix.determinant();
+                } else {
+                    total -= this.getValueAt(0, n) * matrix.determinant();
+                }
+
+            }
+
+            return total;
+        }
     }
 
     @Override
@@ -182,22 +233,22 @@ public class Matrix implements IMatrix {
         return true;
     }
 
-        @Override
-        public boolean isSquareMatrix () {
-            if (this.getRows() == this.getColumns()) {
-                return true;
-            }
-            return false;
+    @Override
+    public boolean isSquareMatrix() {
+        if (this.getRows() == this.getColumns()) {
+            return true;
         }
-
-        @Override
-        public void printToConsole () {
-            for (int i = 0; i < numbers.length; i++) {
-                for (int j = 0; j < numbers[i].length; j++) {
-                    System.out.print(numbers[i][j] + " ");
-                }
-                System.out.println();
-            }
-        }
-
+        return false;
     }
+
+    @Override
+    public void printToConsole() {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
